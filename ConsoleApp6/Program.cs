a914d3b8680e365d5d505afeeaf76e5ffe4c132f
AzTuCode <tu201806158@code.edu.az>
2021-10-28 01:39:17 +0400

    update

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = Console.ReadLine();
+            string result = "";
+         
+            for (int i = value.Length-1; i >= 0; i--)
+            {
+                result += value[i];
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
